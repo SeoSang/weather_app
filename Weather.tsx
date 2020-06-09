97439c1ef33022bd96b4f490220974bee381f922
SeoSang <ddrrpg@naver.com>
2020-06-09 11:26:30 +0900

    강아지사진 기능 추가

--- Weather.tsx
+++ Weather.tsx
@@ -1,10 +1,21 @@
-import React, { Component, FC } from "react"
-import { View, Text, StyleSheet, StatusBar } from "react-native"
+import React, { Component, FC, useState } from "react"
+import { View, Text, StyleSheet, StatusBar, Button, Image } from "react-native"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { LinearGradient } from "expo-linear-gradient"
 import { Condition, weatherData } from "./weatherData"
+import axios from "axios"
+
+const DOG_URL = "https://dog.ceo/api/breeds/image/random"
 
 const Weather: FC<{ temperature: number; condition: Condition }> = ({ temperature, condition }) => {
+  const [image, setImage] = useState("")
+
+  const getDoggy = async () => {
+    const { data } = await axios.get(DOG_URL)
+    console.log(data)
+    setImage(data.message)
+    return data
+  }
   const now = weatherData[condition]
   const innerStyles = StyleSheet.create({
     title: {
@@ -17,6 +28,7 @@ const Weather: FC<{ temperature: number; condition: Condition }> = ({ temperatur
     text: {
       fontSize: now.textSize,
       fontFamily: "hanna",
+      marginBottom: 15,
       color: now.textColor,
     },
     temp: {
@@ -33,10 +45,35 @@ const Weather: FC<{ temperature: number; condition: Condition }> = ({ temperatur
           <MaterialCommunityIcons name={now.iconName} size={96} color={now.iconColor} />
           <Text style={innerStyles.temp}>{temperature}도</Text>
         </View>
-        <View style={styles.halfContainer}>
-          <Text style={innerStyles.title}>{now.title}</Text>
-          <Text style={innerStyles.text}>{now.text}</Text>
-        </View>
+        {image == "" ? (
+          <View style={styles.halfContainer}>
+            <Text style={innerStyles.title}>{now.title}</Text>
+            <Text style={innerStyles.text}>{now.text}</Text>
+            <Button
+              title='오늘의 강아지'
+              onPress={() => {
+                getDoggy()
+              }}
+            ></Button>
+          </View>
+        ) : (
+          <View style={styles.halfContainer}>
+            <Image
+              style={styles.logo}
+              source={{
+                uri: image,
+              }}
+              resizeMode='contain'
+            />
+            <Button
+              title='돌아가기'
+              onPress={() => {
+                console.log(image)
+                setImage("")
+              }}
+            ></Button>
+          </View>
+        )}
       </View>
     </LinearGradient>
   )
@@ -47,6 +84,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    padding: 5,
   },
   temp: {
     fontSize: 32,
@@ -59,6 +100,13 @@ const styles = StyleSheet.create({
   textContainer: {
     paddingHorizontal: 20,
     alignItems: "flex-start",
+  },
+  logo: {
+    width: 300,
+    height: 240,
+  },
+  button: {
+    marginTop: 10,
   },
 })
 
